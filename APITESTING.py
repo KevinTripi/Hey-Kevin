5d352281feb0cbf40b4e5457586db34a3d9c5185
maratsalem <marasalem333@gmail.com>
2025-01-15 13:21:25 -0500

    display text results able to be found

--- APITESTING.py
+++ APITESTING.py
@@ -6,18 +6,17 @@ BASE_URI = 'https://api.bing.microsoft.com/v7.0/images/visualsearch'
 """ sign up for a free student azure account or use the S1 plan to 
     search for things. make a bing search resource, and make it's
     resource group at the same time (or just make sure the group is a
-    visual search resource)
-"""
+    visual search resource) """
 
-""" SUBSCRIPTION_KEY = 'insert API KEY' """ 
-imagePath = "alice.jpg"
+SUBSCRIPTION_KEY = 'c898877efe6e4226851556beef3a5050' 
+imagePath = "dasani-water-217886-64_600.jpg"
 
 HEADERS = {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
 
 file = {'image': ('myfile', open(imagePath, 'rb'))}
 
 def print_json(obj):
-    """Print the object as JSON"""
+    #Print the object as JSON
     print(json.dumps(obj, sort_keys=True, indent=2, separators=(',', ': ')))
 
 try:
@@ -30,16 +29,15 @@ except Exception as ex:
     raise ex 
 
 # Function 
-def get_entries(file_path):
+def get_title_names(file_path):
     entries = []
 
-    
     # Read the JSON file
     with open(file_path, "r") as f:
         data = json.load(f)
     
     # tags -> actions -> data -> value
-    # need to check path for displayText (use displayText data as well?)
+    
     tags = data.get("tags", [])
     for tag in tags:
         actions = tag.get("actions", [])
@@ -54,9 +52,34 @@ def get_entries(file_path):
                         })
     
     return entries
+            
+def get_display_text(file_path):
+    entries = []
+    
+    # Read the JSON file
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    
+    # tags -> actions -> data -> value    
+    tags = data.get("tags", [])
+    for tag in tags:
+        actions = tag.get("actions", [])
+        for action in actions:
+            if action.get("_type") == "ImageRelatedSearchesAction": 
+                value_list = action.get("data", {}).get("value", [])
+                
+                for item in value_list:
+                    displayText = item.get("displayText", "")
+                    entries.append({
+                            "displayText": displayText,
+                        })
+    
+    return entries
 
 file_path = "result.json"
 
-# Extract and print Florence-related entries
-entries = get_entries(file_path)
-print("start",entries,"end please show that this is the end please")
+entries1 = get_title_names(file_path)
+entries2 = get_display_text(file_path)
+
+print("start",entries1,"end please show that this is the end please")
+print("\n now this is the start of second entries", entries2, "this is where entries2 ends")
