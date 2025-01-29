f7f91ddc69597dc1636a2de67be357f1acd9fb44
Dwight-V <dvight@duck.com>
2025-01-29 12:07:09 -0500

    main.dart: ListView formatted slightly better

--- lib/main.dart
+++ lib/main.dart
@@ -81,19 +81,37 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           axisDirection: AxisDirection.up,
 
           // The drawer holds the ListView where our results will sit.
-          drawer: ListView.builder(
-            itemCount: 20,
+          drawer: ListView(
             physics:
                 NeverScrollableScrollPhysics(), // From https://stackoverflow.com/a/51367188
             shrinkWrap:
                 true, // From https://www.flutterbeads.com/listview-inside-column-in-flutter/
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(textAlign: TextAlign.center, "Text ${index}"),
-                // https://api.flutter.dev/flutter/material/ListTile/selected.html
+            children: [
+              // https://api.flutter.dev/flutter/material/ListTile/selected.html
+
+              // Title Tile
+              ListTile(
+                title: Text(textAlign: TextAlign.center, "Brownies"),
+                subtitle: Text('I hurt myself thank you'),
                 // onTap: () {},
-              );
-            },
+              ),
+              // Fact #1 Tile
+              ListTile(
+                title: Text(textAlign: TextAlign.center, "Fact 1"),
+                subtitle: Text('Something about the object\'s color.'),
+              ),
+              // Fact #2 Tile
+              ListTile(
+                title: Text(textAlign: TextAlign.center, "Fact 2"),
+                subtitle: Text('Fake Review'),
+              ),
+              // Fact #3 Tile
+              ListTile(
+                title: Text(textAlign: TextAlign.center, "Fact 3"),
+                subtitle: Text(
+                    'Whoop, whoop, whoop, whoopum gundam style (AI freestyle)'),
+              ),
+            ],
           ),
 
           // The body holds the camera and it's buttons.
