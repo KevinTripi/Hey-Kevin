194118f956d9b8c5b3b370ba93cd89af8dc56749
Dwight-V <dvight@duck.com>
2025-03-09 18:38:43 -0400

    custom_painter: start work on integrating opencv mask

--- lib/widgets/custom_painter.dart
+++ lib/widgets/custom_painter.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 // From https://codewithandrea.com/videos/flutter-custom-painting-do-not-fear-canvas/
 class ObjOutliner extends CustomPainter {
+  /* 
+  I believe that images (in openCV) are represented in a 2D numpy array.
+  Each index is an int[3] with an index for this pixel's red, blue, and green values.
+  In other words, if imgFromCV = int[height][width][3], imgFromCV[i][j] = [b, g, r] (for that specific pixel)
+  Also note it wouldn't be an int[][][], but a uint8[][][].
+  */
+  // var outlineArr;
+
+  // ObjOutliner(outlineArr) {
+  //   this.outlineArr;
+  // }
+
   // Where all the paint stuff goes
   @override
   void paint(Canvas canvas, Size size) {
@@ -12,8 +24,15 @@ class ObjOutliner extends CustomPainter {
 
     // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, paint);
 
+    // Draws cross to show dimensions of painter
     canvas.drawLine(Offset.zero, Offset(size.width, size.height), paint);
     canvas.drawLine(Offset(size.width, 0), Offset(0, size.height), paint);
+
+    // for (int i = 0; i < outlineArr.length - 1; i++) {
+    //   var p1 = Offset(outlineArr[0])
+    //   if (outlineArr[i] < )
+    //   canvas.drawLine(p1, p2, paint)
+    // }
   }
 
   // For if was stateless
