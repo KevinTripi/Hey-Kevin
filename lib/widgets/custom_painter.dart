2402beac644c3d79e473b0c043e09b7e848b0841
Dwight-V <dvight@duck.com>
2025-03-09 20:23:54 -0400

    custom_painter: Initial visiual bubbles for the comments

--- lib/widgets/custom_painter.dart
+++ lib/widgets/custom_painter.dart
@@ -8,11 +8,10 @@ class ObjOutliner extends CustomPainter {
   In other words, if imgFromCV = int[height][width][3], imgFromCV[i][j] = [b, g, r] (for that specific pixel)
   Also note it wouldn't be an int[][][], but a uint8[][][].
   */
-  // var outlineArr;
+  // In the format of (xVal, yVal)
+  var point1, point2;
 
-  // ObjOutliner(outlineArr) {
-  //   this.outlineArr;
-  // }
+  ObjOutliner(this.point1, this.point2);
 
   // Where all the paint stuff goes
   @override
@@ -22,17 +21,59 @@ class ObjOutliner extends CustomPainter {
       ..strokeWidth = 4.0
       ..color = Colors.yellow;
 
-    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, paint);
-
     // Draws cross to show dimensions of painter
-    canvas.drawLine(Offset.zero, Offset(size.width, size.height), paint);
-    canvas.drawLine(Offset(size.width, 0), Offset(0, size.height), paint);
+    // canvas.drawLine(Offset.zero, Offset(size.width, size.height), paint);
+    // canvas.drawLine(Offset(size.width, 0), Offset(0, size.height), paint);
+
+    var offset1 = Offset(point1[0] % size.width, point1[1] % size.height / 2);
+    var offset2 = Offset(point2[0] % size.width, point2[1] % size.height / 2);
+
+    canvas.drawCircle(offset1, 10, paint);
+
+    canvas.drawLine(
+        offset1, customTextPaint(canvas, size, offset2, "test", paint), paint);
 
     // for (int i = 0; i < outlineArr.length - 1; i++) {
     //   var p1 = Offset(outlineArr[0])
     //   if (outlineArr[i] < )
     //   canvas.drawLine(p1, p2, paint)
     // }
+  }
+
+  Offset customTextPaint(
+      Canvas canvas, Size size, Offset offset, String text, Paint paint) {
+    var painter = TextPainter(
+        text: TextSpan(
+            text: text,
+            style: TextStyle(
+              color: paint.color,
+              fontSize: 50,
+            )),
+        textDirection: TextDirection.ltr);
+    painter.layout();
+
+    Offset newOffset = offsetOnscreen(
+        offset, Size(painter.width + 10, painter.height + 10), size);
+
+    canvas.drawRect(
+        Rect.fromLTWH(
+          newOffset.dx - 5,
+          newOffset.dy - 5,
+          painter.size.width + 10,
+          painter.size.height + 10,
+        ),
+        paint);
+    painter.paint(canvas, newOffset);
+
+    return newOffset;
+  }
+
+  Offset offsetOnscreen(Offset offset, Size objSize, Size canvasSize) {
+    if (offset.dx + objSize.width < canvasSize.width) {
+      return offset;
+    } else {
+      return Offset(canvasSize.width - objSize.width, offset.dy);
+    }
   }
 
   // For if was stateless
