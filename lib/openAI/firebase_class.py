fa0760b11ae527ab887c98cb32313565caaf1440
AmmarAMinhas <ammar2003.abid@gmail.com>
2025-04-16 19:08:47 -0400

    Firebase setup complete w anonymous authentication. Also added a docx file with firebase research

--- lib/openAI/firebase_class.py
+++ lib/openAI/firebase_class.py
@@ -1,38 +1,44 @@
 import requests
 
 class FirebaseRestAPI:
-    def __init__(self, db_url):
+    def __init__(self, db_url, id_token):
         self.db_url = db_url.rstrip("/")
+        self.id_token = id_token
 
     # GET all data under "objects"
-    def get(self):
-        response = requests.get(f"{self.db_url}/objects.json")
-        if response.status_code == 200:
+    # This function returns a json with pairs in alphabetical order of keys
+    def get(self, include_keys=False):
+        response = requests.get(f"{self.db_url}/objects.json?auth={self.id_token}")
+        if response.status_code == 200 and not include_keys and response.json():
+            return list(response.json().values())[::-1]
+        elif response.status_code == 200 and include_keys:
             return response.json()
-        return None
+        else:
+            return None
 
     # POST new data under "objects" (auto-generates key)
     def post(self, data):
-        response = requests.post(f"{self.db_url}/objects.json", json=data)
+        response = requests.post(f"{self.db_url}/objects.json?auth={self.id_token}", json=data)
         if response.status_code == 200:
-            return data["d"]        # Needs to be updated
-        print("empty data")
+            return data["object_name"]
         return None
 
-    # DELETE entry by key under "objects". Not to be used outside the class
+    # DELETE entry by key under "objects".
+    # NOT TO BE USED OUTSIDE THIS CLASS
     def delete(self, key):
-        response = requests.delete(f"{self.db_url}/objects/{key}.json")
+        response = requests.delete(f"{self.db_url}/objects/{key}.json?auth={self.id_token}")
         return response.status_code == 200
 
     # DELETE all entries under "objects"
     def delete_all(self):
-        response = requests.put(f"{self.db_url}/objects.json", json={})
+        response = requests.put(f"{self.db_url}/objects.json?auth={self.id_token}", json={})
         return response.status_code == 200
 
     # DELETE the most recent object using Firebase-generated key
     def delete_last_entry(self):
-        all_objects = self.get()
+        all_objects = self.get(True)
         if not all_objects:
             return False
-        latest_key = sorted(all_objects.keys())[-1]
+        latest_key = max(all_objects.keys())
         return self.delete(latest_key)
+
--- lib/openAI/test_firebase.py
+++ lib/openAI/test_firebase.py
@@ -1,31 +1,57 @@
 from firebase_class import FirebaseRestAPI
-from firebase_url import url
+from firebase_variables import *
+import requests
 
-db = FirebaseRestAPI(url)
+# This function generates uid and id_token for authentication to firebase using anonymous login
+def get_anonymous_id_token():
+    url = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={API_KEY}"
+    payload = {"returnSecureToken": True}
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "id_token": data["idToken"],
+            "uid": data["localId"]
+        }
+    else:
+        print("Unable to generate anonymous uid and id_token")
+        return None
 
-json = {"b": "2", "c": "3"}
+# To create an instance of firebase
+db = FirebaseRestAPI(HEY_KEVIN_URL, ID_TOKEN)
 
-new_json = {"d": "1"}
-new_json["e"] = json["b"]
-new_json["f"] = json["c"]
-posted_name = db.post(new_json)
-if posted_name:
-    print(f"Added new entry with name {posted_name}")
-else:
-    print("Failed to add to database")
+# This function posts an entry to firebase
+# Only to be used after GPT/Claude generates comments
+def test_post():
+    json = {"object_name": "1" , "b": "5", "c": "6"}
+    posted_name = db.post(json)
+    if posted_name:
+        print(f"Added new entry to database with name {posted_name}")
+    else:
+        print("Failed to add the entry to database")
 
-# get_json = db.get()
-# for i in get_json:
-#     print(get_json[i])
+# This function shows all entries in firebase
+# For GUI: Reorder keys in order: 2nd key, 3rd key, 1st key
+def show_history():
+    get_json = db.get()
+    if get_json:
+        for entry in get_json:
+            print(entry)
+    else:
+        print("Crickets everywhere...")
 
-# if db.delete_all():
-#     print(f"Deleted all")
-# else:
-#     print("Failed to delete all")
-#
-# get_json = db.get()
-# if get_json:
-#     for i in get_json:
-#         print(get_json[i])
-# else:
-#     print("Nothing in firebase")
+
+# This function clears all entries of firebase
+def clear_history():
+    if db.delete_all():
+        print(f"Deleted all history")
+    else:
+        print("Failed to delete all history")
+
+
+# This function clears last entry in firebase
+def clear_last_entry_in_history():
+    if db.delete_last_entry():
+        print("Deleted latest entry")
+    else:
+        print("Failed to delete latest entry")
